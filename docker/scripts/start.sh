d52ba000a3d4b83c0cb1fea223de7e177efeda5b
Frank Celler <f.celler@triagens.de>
2014-12-17 00:07:30 +0100

    added GiantSwarm description

--- docker/scripts/start.sh
+++ docker/scripts/start.sh
@@ -8,19 +8,24 @@ else
   # sanity check
   export ARANGODB_SERVER=http://localhost:8529
 
-  if [ -z "$DB_LINK_PORT_8529_TCP_ADDR" ];  then
-    echo "warning: DB_LINK_PORT_8529_TCP_ADDR env variable is not set, please link the ArangoDB with '--link instancename:db-link'"
-    exit 1
-  else
+  if [ -n "$GUESSER_BACK_END_PORT_8529_TCP_ADDR" ];  then
+    ARANGODB_SERVER="http://${GUESSER_BACK_END_PORT_8529_TCP_ADDR}:8529"
+    ARANGODB_ENDPOINT="$GUESSER_BACK_END_PORT_8529_TCP"
+  elif [ -n "$DB_LINK_PORT_8529_TCP_ADDR" ];  then
     ARANGODB_SERVER=http://${DB_LINK_PORT_8529_TCP_ADDR}:8529
+    ARANGODB_ENDPOINT="$DB_LINK_PORT_8529_TCP"
+  else
+    echo "warning: DB_LINK_PORT_8529_TCP_ADDR env variable is not set, please link the ArangoDB with '--link instancename:db-link'"
+    env | sort
+    exit 1
   fi
 
   if test "$init" = 1;  then
-    echo "Going to initialize the database at $DB_LINK_PORT_8529_TCP"
+    echo "Going to initialize the database at $ARANGODB_ENDPOINT"
 
-    foxx-manager --server.endpoint $DB_LINK_PORT_8529_TCP fetch zip /install/guesser-foxx.zip
-    foxx-manager --server.endpoint $DB_LINK_PORT_8529_TCP mount guesser /guesser
-    foxx-manager --server.endpoint $DB_LINK_PORT_8529_TCP setup /guesser
+    foxx-manager --server.endpoint $ARANGODB_ENDPOINT fetch zip /install/guesser-foxx.zip
+    foxx-manager --server.endpoint $ARANGODB_ENDPOINT mount guesser /guesser
+    foxx-manager --server.endpoint $ARANGODB_ENDPOINT setup /guesser
   fi
 fi
 
